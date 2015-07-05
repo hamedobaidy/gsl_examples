9d986ebdb82d06a31267375accb49d97a7443650
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-05 20:30:32 +0430

    Adding gsl_vecs_mats

---
+++ gsl_vecs_mats/src/gsl_blocks_example.c
@@ -0,0 +1,21 @@
+/*
+ * gsl_blocks_example.c
+ *
+ *  Created on: Jul 5, 2015
+ *      Author: hamed
+ */
+
+#include <stdio.h>
+#include <gsl/gsl_block.h>
+
+int main(void) {
+	gsl_block * b = gsl_block_alloc(100);
+
+	printf("length of block = %u\n", b->size);
+	printf("block data address = %#x\n", b->data);
+
+	gsl_block_free(b);
+
+	return 0;
+}
+
---
+++ gsl_vecs_mats/src/gsl_vector_read.c
@@ -0,0 +1,28 @@
+/*
+ * gsl_vector_read.c
+ *
+ *  Created on: Jul 5, 2015
+ *      Author: hamed
+ */
+
+#include <stdio.h>
+#include <gsl/gsl_vector.h>
+
+int main(void) {
+	int i;
+	gsl_vector * v = gsl_vector_alloc(10);
+
+	{
+		FILE * f = fopen("test.dat", "r");
+		gsl_vector_fscanf(f, v);
+		fclose(f);
+	}
+
+	for (i = 0; i < 10; i++) {
+		printf("%g\n", gsl_vector_get(v, i));
+	}
+
+	gsl_vector_free (v);
+	return 0;
+}
+
