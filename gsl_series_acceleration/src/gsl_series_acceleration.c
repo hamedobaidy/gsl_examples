fc8ce66eb6135a165409efeab3196530070644d7
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-07 15:10:19 +0430

    Adding gsl_series_acceleration

---
+++ gsl_series_acceleration/src/gsl_series_acceleration.c
@@ -0,0 +1,55 @@
+/*
+ ============================================================================
+ Name        : gsl_series_acceleration.c
+ Author      : Hamed
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sum.h>
+
+#define N 20
+
+int main(void) {
+	double t[N];
+	double sum_accel, err;
+
+	double sum = 0;
+	int n;
+
+	gsl_sum_levin_u_workspace * w = gsl_sum_levin_u_alloc(N);
+
+	const double zeta_2 = M_PI * M_PI / 6.0;
+
+	/* terms for zeta(2) = \sum_{n=1}^{\infty} 1/n^2 */
+
+	for (n = 0; n < N; n++) {
+		double np1 = n + 1.0;
+		t[n] = 1.0 / (np1 * np1);
+		sum += t[n];
+	}
+
+	gsl_sum_levin_u_accel(t, N, w, &sum_accel, &err);
+
+	printf("term-by-term sum = % .16f using %d terms\n", sum, N);
+
+	printf("term-by-term sum = % .16f using %d terms\n", w->sum_plain,
+			w->terms_used);
+
+	printf("exact value		= % .16f\n", zeta_2);
+	printf("accelerated sum = % .16f using %d terms\n", sum_accel,
+			w->terms_used);
+
+	printf("estimated error = % .16f\n", err);
+	printf("actual error	= % .16f\n", sum_accel - zeta_2);
+
+	gsl_sum_levin_u_free(w);
+
+	return EXIT_SUCCESS;
+}
