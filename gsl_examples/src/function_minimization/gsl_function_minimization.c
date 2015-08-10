492f2652b52cc9e7675a20670f5d9d1458f195ce
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-10 17:30:25 +0430

    comment and format

--- gsl_examples/src/function_minimization/gsl_function_minimization.c
+++ gsl_examples/src/function_minimization/gsl_function_minimization.c
@@ -4,7 +4,7 @@
  Author      : Hamed
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : GNU Scientific Library - function minimization example
  ============================================================================
  */
 
