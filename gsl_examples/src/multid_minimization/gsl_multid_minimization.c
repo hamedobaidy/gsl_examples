590cafeddecbe68a1e71a5e31a9fc80aee110de2
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-12 06:52:21 +0430

    comment

--- gsl_examples/src/multid_minimization/gsl_multid_minimization.c
+++ gsl_examples/src/multid_minimization/gsl_multid_minimization.c
@@ -3,8 +3,8 @@
  Name        : gsl_multid_minimization.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - multi-dimensional minimization
  ============================================================================
  */
 
