729e3562b2dcf53c3f13a7e2084395541192abb6
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-13 23:26:23 +0430

    comment

--- gsl_examples/src/multiset_example/gsl_multiset_example.c
+++ gsl_examples/src/multiset_example/gsl_multiset_example.c
@@ -3,8 +3,8 @@
  Name        : gsl_multiset_example.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - multiset example
  ============================================================================
  */
 
