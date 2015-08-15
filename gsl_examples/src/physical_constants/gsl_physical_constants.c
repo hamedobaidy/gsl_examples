1b4ee71da4334899eec6998f2027cf4466f112d3
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-15 15:18:09 +0430

    comments

--- gsl_examples/src/physical_constants/gsl_physical_constants.c
+++ gsl_examples/src/physical_constants/gsl_physical_constants.c
@@ -3,8 +3,8 @@
  Name        : gsl_physical_constants.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - physical constants example
  ============================================================================
  */
 
