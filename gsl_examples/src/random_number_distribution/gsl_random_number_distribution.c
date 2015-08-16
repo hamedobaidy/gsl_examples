73d048c2ade600cfd56c6bacaa4a9ef3fd640d31
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-16 19:04:01 +0430

    comments

--- gsl_examples/src/random_number_distribution/gsl_random_number_distribution.c
+++ gsl_examples/src/random_number_distribution/gsl_random_number_distribution.c
@@ -3,8 +3,8 @@
  Name        : gsl_random_number_distribution.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - random number distribution example
  ============================================================================
  */
 
