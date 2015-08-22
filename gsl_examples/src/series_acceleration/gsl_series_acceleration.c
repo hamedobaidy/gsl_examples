b7c7ee5ea908840b0d9d434772ef1c9dfe16b963
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-22 22:57:29 +0430

    comments

--- gsl_examples/src/series_acceleration/gsl_series_acceleration.c
+++ gsl_examples/src/series_acceleration/gsl_series_acceleration.c
@@ -3,8 +3,8 @@
  Name        : gsl_series_acceleration.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - series acceleration example
  ============================================================================
  */
 
