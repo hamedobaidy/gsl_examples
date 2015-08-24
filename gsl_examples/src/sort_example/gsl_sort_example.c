01b213fb3ffd2287d2749eb29e5f5de40892e846
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-24 16:40:43 +0430

    comments

--- gsl_examples/src/sort_example/gsl_sort_example.c
+++ gsl_examples/src/sort_example/gsl_sort_example.c
@@ -3,8 +3,8 @@
  Name        : gsl_sort_example.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - sort example
  ============================================================================
  */
 
