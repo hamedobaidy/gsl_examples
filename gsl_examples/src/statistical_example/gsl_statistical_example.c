1cded1c6cabb312caf2dad4e638dab738fe35394
hamedobaidy <hamed.obaidy@gmail.com>
2015-08-25 16:24:58 +0430

    comments

--- gsl_examples/src/statistical_example/gsl_statistical_example.c
+++ gsl_examples/src/statistical_example/gsl_statistical_example.c
@@ -3,8 +3,8 @@
  Name        : gsl_statistical_example.c
  Author      : Hamed
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   : GNU General Public License
+ Description : GNU Scientific Library - statistical example
  ============================================================================
  */
 
